cb867e05e1bbe34063ef7defac71068bea1eb9d6
Gilles Coolen <gilleswhc@gmail.com>
2020-06-24 09:30:46 +0200

    Add more quotes

--- src/controller.ts
+++ src/controller.ts
@@ -1,25 +1,10 @@
 import { Quote } from './types.ts';
 
-const quotes: Array<Quote> = [
-  {
-    quote: 'People haven\'t always been there for me but music always has.'
-  },
-  {
-    quote: 'I suffer from girlnextdooritis where the guy is friends with you and that\'s it.'
-  },
-  {
-    quote: 'This is a new year. A new beginning. And things will change.'
-  },
-  {
-    quote: 'No matter what happens in life, be good to people. Being good to people is a wonderful legacy to leave behind.'
-  },
-  {
-    quote: 'I never want to change so much that people can\'t recognize me.'
-  }
-];
+const quotes: Array<string> = JSON.parse(Deno.readTextFileSync('./data/quotes.json'));
 
 const getQuote = ({ response }: { response: any }) => {
-  response.body = quotes[Math.floor(Math.random() * quotes.length)];
+  const quote: Quote = { quote: quotes[Math.floor(Math.random() * quotes.length)] };
+  response.body = quote;
 };
 
 export { getQuote };
