965970323942f183c04c4cab6f9f480cf27a7558
Gilles <gilleswhc@gmail.com>
2020-09-06 18:00:31 +0200

    Remove old images

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,6 @@ import { Application } from 'https://deno.land/x/oak/mod.ts';
 import router from './router.ts';
 
 const env = config();
-const HOST = env.HOST;
-const PORT = env.PORT;
 
 const app = new Application();
 
@@ -13,5 +11,5 @@ app.use(oakCors({ origin: '*' }));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Listening on port ${PORT}.`);
-await app.listen(`${HOST}:${PORT}`);
+console.log(`Listening on port 8000.`);
+await app.listen({ port: 8000 });
